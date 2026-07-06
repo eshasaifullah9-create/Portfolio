d5110ba8179a3b2bd30edc48823c06fd5bdbbf46
eshasaifullah9-create <eshasaifullah9@gmail.com>
2026-07-05 17:06:25 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    new Typed(".auto-type", {
+        strings: [
+            "Frontend Web Developer",
+            "UI Designer",
+            "JavaScript Developer"
+        ],
+        typeSpeed: 80,
+        backSpeed: 50,
+        backDelay: 1500,
+        loop: true
+    });
+
+});
